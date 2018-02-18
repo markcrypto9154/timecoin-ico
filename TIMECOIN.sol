3bbbd05a284d7e176f3c33630d44d427a0b02e72
AlfaTokenTeam <32179003+AlfaTokenTeam@users.noreply.github.com>
2018-02-18 23:24:45 +0300

    add Token and PreSale contracts

---
+++ TIMECOIN.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.12;
+
+import "./lib/BurnableToken.sol";
+import "./lib/UpgradeableToken.sol";
+
+contract TIMECOIN is BurnableToken, UpgradeableToken {
+
+  string public name;
+  string public symbol;
+  uint public decimals;
+
+  function TIMECOIN(address _owner)  UpgradeableToken(_owner) {
+    name = "TIMECOIN";
+    symbol = "TMC";
+    totalSupply = 2;
+    decimals = 0;
+
+    balances[_owner] = totalSupply;
+  }
+}
